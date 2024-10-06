b63db51464dacb822582b64714bd0cefeb4b68b8
Thomas-Cenci <thcenci@gmail.com>
2024-10-06 19:25:50 -0400

    Back to og site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var id = 'gallery';
 
 function reqListener() {
-  var renderer = new HorizontalRenderer(id);
+  var renderer
+  switch (layoutStyle) {
+    case COLUMNS:
+      renderer = new VerticalRenderer(id);
+      break;
+    case ROWS:
+      renderer = new HorizontalRenderer(id);
+      break;
+    case SQUARES:
+      renderer = new SquareRenderer(id);
+      break;
+  }
   var config = new Config(JSON.parse(this.responseText), configuration);
   renderer.render(config);
   lazyload();
@@ -13,4 +24,9 @@ window.onload = function() {
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", "config.json");
   oReq.send();
+
+  let igElem = document.getElementById('instagram');
+  if (igElem.href === 'https://www.instagram.com/') {
+    document.querySelector('div.footer').remove();
+  }
 };
